dbb3420e5a677377ca7576487a86da2e33fc2708
Ed Summers <ehs@pobox.com>
2020-03-01 10:57:20 +0000

    initial

---
+++ README.md
@@ -0,0 +1,36 @@
+monitor is a little script for monitoring URLs and then reporting any failures
+by email. It's not meant to be general purpose, it's just meant for me :) Maybe
+at some point it would be good to put the configuration in a yaml file or
+something eh?
+
+When you run monitor.py you must make sure SMTP_PASSWORD is set in the environment.
+You can do this in cron like so:
+
+    0 * * * * SMTP_PASSWORD="MY_PASSWORD" /home/ed/Projects/monitor/monitor.py
+
+Configure the sites list with the list of URLs to monitor.
+
+	sites = [
+			{
+					'name': 'mith',
+					'url': 'https://mith.umd.edu',
+					'cache_bust': True
+			},
+			{
+					'name': 'docnow',
+					'url': 'https://demo.docnow.io/api/v1/world',
+					'check': lambda resp: len(json.load(resp)) > 0
+			}
+	]
+
+Each site must have a `name` and a `url`.
+
+If `cache_bust` is set to True then a uniquequery string will be attached to
+the url to prevent a cache from reporting it as ok.
+
+If `check` is supplied it will use the function to determine if the response
+was ok. In this case it's attempting to parse a JSON response and check the
+length of the data structure. If no `check` is supplied the response will just
+be checked that it is 200 OK.
+
+If any of the checks fail an email is sent.
---
+++ monitor.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import json
+import time
+import smtplib
+
+from urllib.request import urlopen
+
+sites = [
+    {
+        'name': 'mith',
+        'url': 'https://mith.umd.edu',
+        'cache_bust': True
+    },
+    {
+        'name': 'docnow',
+        'url': 'https://demo.docnow.io/api/v1/world',
+        'check': lambda resp: False #len(json.load(resp)) > 0
+    }
+]
+
+
+from_email = 'ehs@pobox.com'
+to_email = 'ehs@pobox.com'
+smtp_host = 'smtp.pobox.com'
+smtp_port = 587
+username = 'ehs@pobox.com'
+password = os.environ.get('SMTP_PASSWORD') 
+
+if password is None:
+    sys.exit('You must set the SMTP_PASSWORD environment variable!')
+
+template = '''From: {from_email:s}
+To: {to_email:s}
+Subject: {subject:s}
+
+{body:s}
+'''
+
+def send_email(alert_name, body):
+    try:
+        smtp = smtplib.SMTP(smtp_host, smtp_port)
+        smtp.starttls()
+        smtp.login(username, password)
+        msg = template.format(
+            from_email=from_email,
+            to_email=to_email,
+            subject="Monitor Alert: {}".format(alert_name),
+            body=body
+        )
+        smtp.sendmail(from_email, [to_email], msg)
+    except Exception as e: 
+        print('Failed sending message: {}'.format(e))
+
+def check(f, resp):
+    try:
+        return f(resp)
+    except:
+        return False
+
+# look at sites and report on any failures by email
+
+for site in sites:
+    fail = None
+
+    url = site['url'] 
+    if site.get('cache_bust'):
+        url += "?t={}".format(time.time())
+
+    try:
+        resp = urlopen(url)
+        if site.get('check') and not check(site['check'], resp):
+            fail = 'Check failed.'
+        elif resp.code != 200:
+            fail = "Response code not 200 OK."
+    except Exception as e:
+        fail = str(e)
+
+    if fail is not None:
+        send_email(site['name'], 'Failed to get {0}\n\n{1}'.format(site['url'], fail))
